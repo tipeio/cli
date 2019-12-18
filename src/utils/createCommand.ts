b32bc0bfa4c908987751ff2398b08d86874c4a62
Scott Moss <willscottmoss@gmail.com>
2019-12-18 14:06:36 -0800

    fix(auth): add api call
    Add api call to fetch token for auth

--- src/utils/createCommand.ts
+++ src/utils/createCommand.ts
@@ -2,6 +2,13 @@ import { CommandConfig } from '../types'
 
 export const createCommand = (prog: Caporal, config: CommandConfig): Command => {
   const command = prog.command(config.command, config.description)
+
+  if (config.options?.length) {
+    config.options.forEach(option => {
+      command.option(option.option, option.description, option.type)
+    })
+  }
+
   command.action(config.action)
   return command
 }
