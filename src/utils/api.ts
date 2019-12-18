933fab14e0174b6d400eb48d02af85a6831888db
Scott Moss <willscottmoss@gmail.com>
2019-12-18 14:06:36 -0800

    fix(auth): add api call
    Add api call to fetch token for auth

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,37 @@
-import { Project, Env, NewEnv } from '../types'
+import { Project, Env, NewEnv, APIConfig } from '../types'
+import got from 'got'
+
+const DEV_URL = 'http://localhost:8000'
+const PROD_URL = 'https://api.tipe.io'
+
+const getURL = (dev: boolean): string => (dev ? DEV_URL : PROD_URL)
+
+async function api<T>(options: APIConfig): Promise<T> {
+  try {
+    const config: any = { prefixUrl: getURL(options.dev) }
+
+    if (options.auth) {
+      config.headers = { authorization: `Bearer ${options.auth}` }
+    }
+
+    if (options.payload) {
+      config.json = options.payload
+    }
+
+    if (options.project) {
+      if (config.json) {
+        config.json = { ...config.json, project: options.project }
+      } else {
+        config.json = { project: options.project }
+      }
+    }
+
+    const result: any = await got.post(options.path, config).json()
+    return result.data
+  } catch (e) {
+    throw e
+  }
+}
 
 const wait = (time: number, payload: any): Promise<any> =>
   new Promise(resolve => {
@@ -35,6 +68,9 @@ export const createEnv = (newEnv: NewEnv): Promise<Env> => {
   return wait(2500, env)
 }
 
-export const getAuthToken = (): Promise<string> => wait(1200, '945jfas0934jasd09fkdasna94n')
+export const getAuthToken = async (options: any): Promise<string> => {
+  const result: { token: string } = await api<{ token: string }>({ path: 'cli-token', dev: options.dev })
+  return result.token
+}
 
-export const authenticate = (): Promise<string> => wait(5000, '3948fs934nfa90i4jna9kf8')
+export const authenticate = (options: any): Promise<string> => wait(5000, '3948fs934nfa90i4jna9kf8')
