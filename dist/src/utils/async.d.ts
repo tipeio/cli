3e7f4bd9b099d2a9d7e3a788e33f7dcb14674562
realmike33 <realmike33@gmail.com>
2020-09-02 11:29:38 -0700

    chore(dist): removed dist folder

--- dist/src/utils/async.d.ts
+++
@@ -1,2 +0,0 @@
-import { AsyncWrapped } from '../types';
-export declare function asyncWrap<T>(promise: Promise<T>, errorExt?: object): Promise<AsyncWrapped<T>>;
--- dist/src/utils/install.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const yarnInstall: (libs: string[]) => Promise<any>;
-export declare const npmInstall: (libs: string[]) => Promise<any>;
-export declare const repoInstall: (repo: string, where: string) => Promise<any>;
-export declare const installModules: (libs: string[]) => Promise<any>;
