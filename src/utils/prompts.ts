c8e290468a377a9324492d646d8bd11ca14ed1d1
Scott Moss <willscottmoss@gmail.com>
2020-11-19 00:37:30 -0800

    fix(init): handle error messages

--- src/utils/prompts.ts
+++ src/utils/prompts.ts
@@ -15,101 +15,124 @@ export const askInitQuestions = async (
   handleInitAccount: HandleInitAccountFn,
 ): Promise<ProjectConfig> => {
   let result: InitAccountResult
-  let org, project
+  let org, project, env
 
-  // users first org must be a personal org. No need to ask
-  if (!orgs.length) {
-    result = await handleInitAccount(
-      {
-        orgName: 'Personal',
-        projectName: 'My First Project',
-        envName: 'Prodcution',
-      },
-      true,
-    )
-  } else {
-    const config: InitPromptAnswers = {}
-
-    const { selectedOrg } = await prompts({
-      type: 'select',
-      name: 'selectedOrg',
-      message: 'Select or create an Organization',
-      choices: [
-        { title: 'Create a new Organization', value: { id: 0, projects: [] } },
-        ...orgs.map((org: Organziation) => ({
-          title: chalk.yellow(org.name),
-          value: org,
-        })),
-      ],
-    })
-
-    if (selectedOrg.id === 0) {
-      const { orgName } = await prompts({
-        type: 'text',
-        name: 'orgName',
-        message: 'Give your Organization a name.',
-        initial: 'My Organization',
-      })
-
-      config.orgName = orgName
+  try {
+    if (!orgs.length) {
+      result = await handleInitAccount(
+        {
+          orgName: 'Personal',
+          projectName: 'My First Project',
+          envName: 'Prodcution',
+        },
+        true,
+      )
     } else {
-      config.orgId = selectedOrg.id || selectedOrg._id
-      org = selectedOrg
-    }
+      const config: InitPromptAnswers = {}
 
-    if (org && org.projects.length) {
-      const { selectedProject } = await prompts({
+      const { selectedOrg } = await prompts({
         type: 'select',
-        name: 'selectedProject',
-        message: 'Select or create a Project',
+        name: 'selectedOrg',
+        message: 'Select or create an Organization',
         choices: [
-          { title: 'Create a new Project', value: { id: 0, environments: [] } },
-          ...selectedOrg.projects.map((p: Project) => ({
-            title: chalk.yellow(p.name),
-            value: p,
+          { title: 'Create a new Organization', value: { id: 0, projects: [] } },
+          ...orgs.map((org: Organziation) => ({
+            title: chalk.yellow(org.name),
+            value: org,
           })),
         ],
       })
 
-      if (selectedProject.id === 0) {
+      if (selectedOrg.id === 0) {
+        const { orgName } = await prompts({
+          type: 'text',
+          name: 'orgName',
+          message: 'Give your Organization a name.',
+          initial: 'My Organization',
+        })
+
+        config.orgName = orgName
+      } else {
+        config.orgId = selectedOrg.id || selectedOrg._id
+        org = selectedOrg
+      }
+
+      if (org && org.projects.length) {
+        const { selectedProject } = await prompts({
+          type: 'select',
+          name: 'selectedProject',
+          message: 'Select or create a Project',
+          choices: [
+            { title: 'Create a new Project', value: { id: 0, environments: [] } },
+            ...selectedOrg.projects.map((p: Project) => ({
+              title: chalk.yellow(p.name),
+              value: p,
+            })),
+          ],
+        })
+
+        if (selectedProject.id === 0) {
+          const { projectName } = await prompts({
+            type: 'text',
+            name: 'projectName',
+            message: 'Give your project a name.',
+            initial: 'My Tipe Project',
+          })
+
+          config.projectName = projectName
+        } else {
+          config.projectId = selectedProject.id || selectedProject._id
+          project = selectedProject
+        }
+      } else {
         const { projectName } = await prompts({
           type: 'text',
           name: 'projectName',
-          message: 'Give your project a name.',
+          message: 'Name your first Project',
           initial: 'My Tipe Project',
         })
 
         config.projectName = projectName
-      } else {
-        config.projectId = selectedProject.id || selectedProject._id
-        project = selectedProject
       }
-    } else {
-      const { projectName } = await prompts({
-        type: 'text',
-        name: 'projectName',
-        message: 'Name your first Project',
-        initial: 'My Tipe Project',
-      })
 
-      config.projectName = projectName
-    }
+      if (project && project.environments.length) {
+        const { selectedEnv } = await prompts({
+          type: 'select',
+          name: 'selectedEnv',
+          message: 'Select or create an Environment',
+          choices: [
+            { title: 'Create a new Environment', value: { id: 0 } },
+            ...project.environments.map((e: Env) => ({
+              title: `${e.name} (${e.private ? chalk.red('Private') : chalk.green('Public')})`,
+              value: e,
+            })),
+          ],
+        })
 
-    if (project && project.environments.length) {
-      const { selectedEnv } = await prompts({
-        type: 'select',
-        name: 'selectedEnv',
-        message: 'Select or create an Environment',
-        choices: [
-          { title: 'Create a new Environment', value: { id: 0 } },
-          ...project.environments.map((e: Env) => ({
-            title: `${e.name} (${e.private ? chalk.red('Private') : chalk.green('Public')})`,
-            value: e,
-          })),
-        ],
-      })
+        if (selectedEnv.id === 0) {
+          const { envName } = await prompts({
+            type: 'text',
+            name: 'envName',
+            message: 'Give your environment a name.',
+            initial: 'production',
+          })
 
-      if (selectedEnv.id === 0) {
+          const { envPrivate } = await prompts({
+            type: 'toggle',
+            name: 'envPrivate',
+            message: `Make "${envName}" private? (API Key required to access content)`,
+            initial: false,
+            inactive: 'no',
+            active: 'yes',
+          })
+
+          config.envName = envName
+          config.envPrivate = envPrivate
+        } else {
+          config.envId = selectedEnv.id || selectedEnv._id
+          env = selectedEnv
+        }
+      } else {
         const { envName } = await prompts({
           type: 'text',
           name: 'envName',
@@ -128,41 +151,31 @@ export const askInitQuestions = async (
 
         config.envName = envName
         config.envPrivate = envPrivate
-      } else {
-        config.envId = selectedEnv.id || selectedEnv._id
       }
-    } else {
-      const { envName } = await prompts({
-        type: 'text',
-        name: 'envName',
-        message: 'Give your environment a name.',
-        initial: 'production',
-      })
 
-      const { envPrivate } = await prompts({
-        type: 'toggle',
-        name: 'envPrivate',
-        message: `Make "${envName}" private? (API Key required to access content)`,
-        initial: false,
-        inactive: 'no',
-        active: 'yes',
-      })
-
-      config.envName = envName
-      config.envPrivate = envPrivate
+      if (config.envName) {
+        result = await handleInitAccount(config)
+      } else {
+        result = {
+          org,
+          project,
+          env,
+        }
+      }
     }
 
-    result = await handleInitAccount(config)
+    const { writeEnv } = await prompts({
+      type: 'toggle',
+      name: 'writeEnv',
+      message: 'Should we add tipe env variables to your .env file?',
+      initial: true,
+      inactive: 'no',
+      active: 'yes',
+    })
+
+    return { ...result, writeEnv }
+  } catch (e) {
+    throw new Error('✖ Could not initialize Tipe.')
   }
-
-  const { writeEnv } = await prompts({
-    type: 'toggle',
-    name: 'writeEnv',
-    message: 'Should we add tipe env variables to your .env file?',
-    initial: true,
-    inactive: 'no',
-    active: 'yes',
-  })
-
-  return { ...result, writeEnv }
+  // users first org must be a personal org. No need to ask
 }
